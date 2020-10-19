91013826c6a6bc723ba9d8e28c0c51affb109c6c
Zoey Hu <zoey4y@gmail.com>
2020-10-18 17:51:51 -0700

    Create Is_Valid_Name.rb

---
+++ Intro/Is_Valid_Name.rb
@@ -0,0 +1,34 @@
+# A name is valid is if satisfies all of the following:
+# - contains at least a first name and last name, separated by spaces
+# - each part of the name should be capitalized
+#
+# Hint: use str.upcase or str.downcase
+# "a".upcase # => "A"
+
+def is_valid_name(str)
+  str_arr=str.split(" ")
+  if str_arr.length>=2 && is_capitalized(str)==true
+    return true
+  else
+    return false
+  end
+
+end
+
+
+def is_capitalized(name)
+  name_arr=name.split(" ")
+  name_arr.each do |word|
+    if word!=word[0].upcase+word[1..-1].downcase
+      return false
+    end
+  end
+  
+  return true
+end
+  
+
+puts is_valid_name("Kush Patel")       # => true
+puts is_valid_name("Daniel")           # => false
+puts is_valid_name("Robert Downey Jr") # => true
+puts is_valid_name("ROBERT DOWNEY JR") # => false
