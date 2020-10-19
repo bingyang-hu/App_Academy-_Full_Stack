a24f2ac0a018537c8c39d79d19094effe280331c
Zoey Hu <zoey4y@gmail.com>
2020-10-18 17:54:32 -0700

    Update Reverse_Words.rb

--- Intro/Reverse_Words.rb
+++ Intro/Reverse_Words.rb
@@ -1,3 +1,10 @@
+#Write a method reverse_words that takes in a sentence string
+#and returns the sentence with the order of the characters in each word reversed. 
+#Note that we need to reverse the order of characters in the words,
+#do not reverse the order of words in the sentence.
+
+
+
 def reverse_words(sent)
   word_arr=sent.split(" ")
   new_arr=[]
