ef4dad72d028cbae96b8fc4771cdafdd07594053
Zoey Hu <zoey4y@gmail.com>
2020-10-29 14:02:56 -0700

    Create Frequent_Letters.rb

---
+++ Intro/Hashes/Frequent_Letters.rb
@@ -0,0 +1,28 @@
+# Write a method frequent_letters that takes in a string and returns an array 
+# containing the characters that appeared more than twice in the string
+
+
+
+def frequent_letters(string)
+  count=Hash.new(0)
+  string.each_char do |ele|
+    count[ele]+=1
+  end
+  
+  arr=[]
+  
+  count.each do |k,v| 
+  	if v>2
+      arr<<k
+    end
+   end
+return arr
+
+      
+
+end
+
+print frequent_letters('mississippi') #=> ["i", "s"]
+puts
+print frequent_letters('bootcamp') #=> []
+puts
